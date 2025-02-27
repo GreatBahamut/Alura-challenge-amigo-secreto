977973d0eeba566a715fe1653395e254bc2e2d6d
GreatBahamut <julianbahamut@gmail.com>
2025-02-27 20:26:36 -0300

    constantes del dom declaradas

--- app.js
+++ app.js
@@ -1,1 +1,7 @@
+'use strict'
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+
+const lista = document.getElementById('listaAmigos')
+const resultados = document.getElementById('resultado')
+const botonAnadir = document.querySelector('.button-add')
+const botonSortear = document.querySelector('.button-draw')
